bd1855379068d013141934676331aa863f87a503
poggy-sl <poggy13poggy@gmail.com>
2022-08-12 17:48:15 +0300

    container component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {  Routes, Route } from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 
 const App = (props) => {
@@ -19,8 +20,8 @@ debugger;
         <Navbar />
         <div className="app-wrapper-content">
           <Routes>
-            <Route path="/dialogs/*" element={<Dialogs store={props.store} />} />
-            <Route path="/profile" element={<Profile profilePage={props.state.profilePage} dispatch = {props.dispatch}/>} />
+            <Route path="/dialogs/*" element={<DialogsContainer store={props.store} />} />
+            <Route path="/profile" element={<Profile store={props.store}/>} />
             <Route path="/news" element={<News/>} />
             <Route path="/music" element={<Music/>} />
             <Route path="/settings" element={<Settings/>} />
