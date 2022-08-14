bb333fb2f2356e9a52a50062e52e0c03da511690
poggy-sl <poggy13poggy@gmail.com>
2022-08-14 12:35:44 +0300

    ...state, refactoring dialogs&profileReducers

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -58,21 +58,18 @@ let initialState = {
 const dialogsReducer = (state = initialState, action) => {
 
     switch(action.type){
-        case UPDATE_NEW_MESSAGE_BODY: {
-            let stateCopy = {...state};
-            stateCopy.newMessageBody = action.body;
-            return stateCopy;
-        }
-        case SEND_MESSAGE: {
-            let stateCopy = {...state};
+        case UPDATE_NEW_MESSAGE_BODY: 
+            return{ 
+                ...state, 
+                newMessageBody : action.body
+            }
+        case SEND_MESSAGE: 
             let body = state.newMessageBody;
-            stateCopy.newMessageBody = '';
-            stateCopy.messages.push({
-                id: 7,
-                message: body
-            });
-            return stateCopy;
-        }
+            return{ 
+                ...state, 
+                newMessageBody : '',
+                messages: [...state.messages, {id: 7, message: body}]
+            }
         default:
             return state;
     }
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -39,23 +39,22 @@ let initialState = {
 const profileReducer = (state = initialState, action) => {
     
     switch(action.type) {
-        case ADD_POST: {
+        case ADD_POST: 
             let newPost = {
                 id: 7,
                 message: state.newPostText,
                 likesCount: 0
             };
-            let stateCopy = {...state};
-            stateCopy.posts = [...state.posts];
-            stateCopy.posts.push(newPost);
-            stateCopy.newPostText = '';
-            return stateCopy;
-        }
-        case UPDATE_NEW_POST_TEXT: {
-            let stateCopy = {...state};
-            stateCopy.newPostText = action.newText;
-            return stateCopy;
-        }
+            return{ 
+                ...state,
+                posts: [...state.posts, newPost],
+                newPostText : ''
+            }
+        case UPDATE_NEW_POST_TEXT:
+            return{ 
+                ...state,
+                newPostText : action.newText
+            } 
         default:
             return state;
     }
