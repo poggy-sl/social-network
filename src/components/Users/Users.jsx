3e959e046cee9b606c56d861398d41a3772671a6
poggy-sl <poggy13poggy@gmail.com>
2022-08-16 16:42:00 +0300

    rest api, axios i

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import styles from "./users.module.css";
+import * as axios from "axios";
+import userPhoto from "../../assets/images/png-icon.png";
 
 
 let Users = (props) => {
     if(props.users.length === 0){
-    props.setUsers([
-            {id: 1, followed: false, photoUrl: 'https://mir-optik.ru/i/Stati/tony.png', fullName: 'Dmitriy', status: 'I`m a boss', location: {city: 'Minsk', country: 'Belarus'}},
-            {id: 2, followed: true, photoUrl: 'https://mir-optik.ru/i/Stati/tony.png', fullName: 'Sasha', status: 'I`m a boss too', location: {city: 'Moscow', country: 'Russia'}},
-            {id: 3, followed: false, photoUrl: 'https://mir-optik.ru/i/Stati/tony.png', fullName: 'Andrew', status: 'I`m a boss too', location: {city: 'Kiev', country: 'Ukraine'}}
-        ])
+
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        props.setUsers(response.data.items);
+    })
+    // props.setUsers([
+    //         // {id: 1, followed: false, photoUrl: 'https://mir-optik.ru/i/Stati/tony.png', fullName: 'Dmitriy', status: 'I`m a boss', location: {city: 'Minsk', country: 'Belarus'}},
+    //         // {id: 2, followed: true, photoUrl: 'https://mir-optik.ru/i/Stati/tony.png', fullName: 'Sasha', status: 'I`m a boss too', location: {city: 'Moscow', country: 'Russia'}},
+    //         // {id: 3, followed: false, photoUrl: 'https://mir-optik.ru/i/Stati/tony.png', fullName: 'Andrew', status: 'I`m a boss too', location: {city: 'Kiev', country: 'Ukraine'}}
+    //     ])
     }
     
     return <div>
@@ -16,7 +22,7 @@ let Users = (props) => {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto} />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
                     </div>
                     <div>
                         {u.followed 
@@ -27,13 +33,13 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                 </span>
                 <span>
-                    <div>{u.location.country}</div>
-                    <div>{u.location.city}</div>
+                    <div>{"u.location.country"}</div>
+                    <div>{"u.location.city"}</div>
                 </span>
             </div>)
         }
