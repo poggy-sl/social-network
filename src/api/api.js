ec40a558e4b19c4e00a5027344fab63408b3168e
poggy-sl <poggy13poggy@gmail.com>
2022-08-22 18:08:48 +0300

    follow & unfollow api.js

--- src/api/api.js
+++ src/api/api.js
@@ -12,5 +12,11 @@ export const usersAPI = {
     getUsers(currentPage = 1, pageSize = 10) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`)
         .then(response => response.data);
+    },
+    unfollow(userId){
+        return instance.delete(`follow/${userId}`)
+    },
+    follow(userId){
+        return instance.post(`follow/${userId}`)
     }
 }
