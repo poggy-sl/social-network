0ec22ff0beafac7030982e7d323795084180344b
poggy-sl <poggy13poggy@gmail.com>
2022-08-24 16:27:21 +0300

    auth & profile thunk

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Profile from "./Profile";
-import * as axios from "axios";
 import { connect } from "react-redux";
-import {setUserProfile} from './../../redux/profile-reducer';
+import { getUserProfile } from "../../redux/profile-reducer";
 import {useLocation, useNavigate, useParams} from 'react-router-dom';
+
 
 class ProfileContainer extends React.Component {
 
@@ -14,10 +14,7 @@ class ProfileContainer extends React.Component {
       userId = 2;
     }
 
-    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
-            .then(response => {
-                this.props.setUserProfile(response.data);
-            });
+    this.props.getUserProfile(userId);
   }
 
   render(){
@@ -46,4 +43,5 @@ let withRouter = (Component) => {
 }
 
 
-export default connect(mapStateToProps, {setUserProfile}) (withRouter(ProfileContainer));
+export default connect(mapStateToProps, { getUserProfile })
+(withRouter(ProfileContainer));
