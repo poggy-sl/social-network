2a36e95c1d03a88833846679bf6136f906553a96
poggy-sl <poggy13poggy@gmail.com>
2022-08-27 16:00:33 +0300

    http put, local state

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -4,15 +4,21 @@ import React from 'react';
 class ProfileStatus extends React.Component {
   
   state = {
-    editMode: false
+    editMode: false,
+    status: this.props.status
   }
 
-  activateEditMode(){
+  activateEditMode = () => {
     this.setState({editMode: true});
   }
 
-  deactivateEditMode(){
+  deactivateEditMode = () => {
     this.setState({editMode: false});
+    this.props.updateStatus(this.state.status)
+  }
+
+  onStatusChange = (e) => {
+    this.setState({status: e.currentTarget.value});
   }
 
   render(){
@@ -20,14 +26,15 @@ class ProfileStatus extends React.Component {
       <div>
         {!this.state.editMode &&
           <div>
-            <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+            <span onDoubleClick={this.activateEditMode}>{this.props.status || "Write your status..."}</span>
           </div>
         }
 
         {this.state.editMode &&
           <div>
-            <input onBlur={this.deactivateEditMode.bind(this)} 
-                    value={this.props.status}
+            <input onChange={this.onStatusChange}
+                    onBlur={this.deactivateEditMode} 
+                    value={this.state.status}
                     autoFocus={true}/>
           </div>
         }
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,9 @@
-import {usersAPI} from '../api/api';
+import {usersAPI, profileAPI} from '../api/api';
+
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
+const SET_STATUS = 'SET_STATUS';
 
 let initialState = {
     posts: [{
@@ -36,7 +38,8 @@ let initialState = {
         },
     ],
     newPostText: 'it-kamasutra',
-    profile: null
+    profile: null,
+    status: ""
 }
 
 const profileReducer = (state = initialState, action) => {
@@ -58,6 +61,9 @@ const profileReducer = (state = initialState, action) => {
         case SET_USER_PROFILE: {
             return {...state, profile: action.profile}
         }
+        case SET_STATUS: {
+            return{...state, status: action.status}
+        }
         default:
             return state;
     }
@@ -66,6 +72,7 @@ const profileReducer = (state = initialState, action) => {
 export const addPostActionCreator = () => ({ type: ADD_POST})
 export const updateNewPostTextActionCreator = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text})
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile})
+export const setStatus = (status) => ({type: SET_STATUS, status})
 
 export const getUserProfile = (userId) => {
     return (dispatch) => {
@@ -75,5 +82,22 @@ export const getUserProfile = (userId) => {
             });
     }
 }
+
+export const getStatus = (userId) => (dispatch) => {
+    profileAPI.getStatus(userId)
+        .then(response => {
+            dispatch(setStatus(response.data));
+        })
+}
+
+export const updateStatus = (status) => (dispatch) => {
+    profileAPI.updateStatus(status)
+        .then(response => {
+            if (response.data.resultCode === 0) {
+                dispatch(setStatus(status));
+            }
+        });
+}
+
 
 export default profileReducer;
